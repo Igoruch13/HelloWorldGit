ac91caa64e6d51b0a2e67e65386d1908d8ce083f
Igoruch13 <piv13piv@gmail.com>
2021-10-19 22:43:13 +0300

    Chek

--- src/HelloWorldGit.java
+++ src/HelloWorldGit.java
@@ -4,5 +4,6 @@ public class HelloWorldGit {
     public static void main(String[] args) {
 
         System.out.println("Hello world to GitHub!!!");
+        System.out.println("Hello!!!");
     }
 }
