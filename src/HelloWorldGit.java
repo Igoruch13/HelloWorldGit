68e077e2f25a40b96c4bf9295dc8e3f1db636154
Igoruch13 <piv13piv@gmail.com>
2021-10-20 00:07:33 +0300

    Chek3

--- src/HelloWorldGit.java
+++ src/HelloWorldGit.java
@@ -1,8 +1,25 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
 
 public class HelloWorldGit {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world to GitHub!!!");
+        Scanner s = new Scanner(System.in);
+        int chuslo = 0;
+        int kilkistSprob = 0;
+        do {
+            try {
+                System.out.println("Введіть число 10");
+                chuslo = s.nextInt();
+                System.out.println("Ви ввели число:" + chuslo);
+            } catch (Exception catchException) {
+                System.out.println("Такого робити неможна!!!");
+                chuslo = 0;
+            }
+            kilkistSprob++;
+            System.out.println("Спроба №:" + kilkistSprob);
+        } while (chuslo != 10);
+        System.out.println("Ну нарешті ввели число 10 з " + kilkistSprob + " спроб!!!");
     }
 }
